05ff110e01675067a072582d64329b79ac0642b9
unknown <levantuan6898@gmail.com>
2020-04-26 15:49:17 +0700

    add recursion

--- src/BinarySearch_WithRecursive.java
+++ src/BinarySearch_WithRecursive.java
@@ -2,21 +2,16 @@ public class BinarySearch_WithRecursive {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,8,9};
         int searchValue = 4;
-        int index = binarySearch(arr,searchValue);
+        int index = binarySearch(arr,0,arr.length-1,searchValue);
         if(index!= -1){
             System.out.println("Element found at index: " + index);
         }else System.out.println("Not found");
     }
-    public static int binarySearch(int[] arr, int x){
-        int left = 0;
-        int right = arr.length-1;
-        while (left<=right){
-            int mid = (left+right)/2;
-            if(x==arr[mid]) return mid;
-            else if(x<arr[mid]) right = mid-1;
-            else left = mid +1;
-
-        }
-        return -1;
+    public static int binarySearch(int[] arr,int left, int right, int x){
+        if(left>right) return -1;
+        int mid = (left+right)/2;
+        if(x == arr[mid]) return mid;
+        else if(x<arr[mid])return binarySearch(arr,left,mid-1,x);
+        else return binarySearch(arr,mid+1, right,x);
     }
 }
